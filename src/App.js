22eff75132f6f91ac326397bcf88ac4396185f4b
Daniel Lim <dlim87@gmail.com>
2018-09-26 13:12:50 -0700

    working 2d version

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Board from './components/board.js'
 
-//NOTE: See below for pseudo-code
-
 class App extends Component {
   constructor(props){
     super(props)
